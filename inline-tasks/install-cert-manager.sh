486449d0e9f4d4bbb590ce17ecdf58ac3358123b
Mario Nitchev <marionitchev@gmail.com>
2020-11-09 15:42:54 +0000

    WIP - squash later...
    Co-authored-by: Kieron Browne <kbrowne@vmware.com>

--- inline-tasks/install-cert-manager.sh
+++ inline-tasks/install-cert-manager.sh
@@ -20,4 +20,55 @@ install-cert-manager() {
     --set installCRDs=true
 }
 
+configure-certs() {
+  key_file=$(mktemp)
+  echo "$DNS_SERVICE_ACCOUNT_JSON" >"$key_file"
+  kubectl create secret generic clouddns-dns01-solver-svc-acct --from-file="$key_file"
+  rm "$key_file"
+
+  cert_config_file=$(mktemp)
+  cat <<EOF >>"$cert_config_file"
+---
+apiVersion: cert-manager.io/v1
+kind: Issuer
+metadata:
+  name: letsencrypt-staging
+  namespace: istio-system
+spec:
+  acme:
+    email: eirini@cloudfoundry.org
+    server: https://acme-staging-v02.api.letsencrypt.org/directory
+    privateKeySecretRef:
+      name: letsencrypt-staging-private-key
+    solvers:
+      - dns01:
+          cloudDNS:
+            # The ID of the GCP project
+            project: "$GCP_PROJECT_ID"
+            # This is the secret used to access the service account
+            serviceAccountSecretRef:
+              name: clouddns-dns01-solver-svc-acct
+              key: key.json
+
+---
+apiVersion: cert-manager.io/v1
+kind: Certificate
+metadata:
+  name: eirinidotcf-cert
+  namespace: istio-system
+spec:
+  secretName: eirinidotcf-cert
+  commonName: eirini.cf
+  dnsNames:
+  - eirini.cf
+  - '*.apps.cf4k8s4a8e.ci-envs.eirini.cf-app.com'
+  issuerRef:
+    name: letsencrypt-staging
+EOF
+
+  kubectl apply -f "$cert_config_file"
+  rm "$cert_config_file"
+}
+
 install-cert-manager
+configure-certs
