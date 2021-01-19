bc7805a6a7ca487f4604edf869d52585d5e35e56
Kieron Browne <kbrowne@vmware.com>
2021-01-19 17:15:36 +0000

    Config that deploys a working postfacto
    [#174272728]
    
    Co-authored-by: Giuseppe Capizzi <gcapizzi@pivotal.io>

--- inline-tasks/deploy-postfacto-cf4k8s.sh
+++ inline-tasks/deploy-postfacto-cf4k8s.sh
@@ -4,6 +4,11 @@ set -euo pipefail
 export KUBECONFIG="$PWD/kube/config"
 export GOOGLE_APPLICATION_CREDENTIALS="$PWD/kube/service-account.json"
 
+redis_password="$(cat redis-password/password)"
+values=eirini-private-config/environments/kube-clusters/cf4k8s4a8e/default-values.yml
+cf_domain="$(goml get -f "$values" -p "system_domain")"
+cf_admin_password="$(goml get -f "$values" -p "cf_admin_password")"
+
 unzip postfacto/package.zip -d postfacto/
 cp eirini-private-config/postfacto-deployment/api/config.js postfacto/package/assets/client/
 
@@ -15,68 +20,30 @@ sed -i "34igem 'execjs'" postfacto/package/assets/Gemfile
 # echo "web:  bundle exec rake db:migrate && bundle exec rails s -p \$PORT -e development" >postfacto/package/assets/Procfile
 rm postfacto/package/assets/.ruby-version
 
-sed -i "32i  config.hosts << /retro.*\.cf4k8s4a8e\.ci-envs\.eirini\.cf-app\.com/" postfacto/package/assets/config/environments/development.rb
+sed -i "32i  config.hosts << 'retro.${cf_domain}'" postfacto/package/assets/config/environments/development.rb
+sed -i "32i  config.hosts << 'retro.eirini.cf'" postfacto/package/assets/config/environments/development.rb
 
-redis_password="$(cat redis-password/password)"
-values=eirini-private-config/environments/kube-clusters/cf4k8s4a8e/default-values.yml
-cf_domain="$(goml get -f "$values" -p "system_domain")"
-cf_admin_password="$(goml get -f "$values" -p "cf_admin_password")"
 cf api "api.$cf_domain" --skip-ssl-validation
 cf auth admin "$cf_admin_password"
 cf create-org postfacto
 cf create-space -o postfacto postfacto
 cf target -o postfacto -s postfacto
 
-# domain="$(goml get -f "$values" -p "eirini.opi.ingress_endpoint")"
-domain="$cf_domain"
-
-if cf app postfacto-api-old; then
-  cf delete -f postfacto-api-old
-fi
-
-if cf app postfacto-api; then
-  cf rename postfacto-api postfacto-api-old
-fi
-
 cf push -f eirini-private-config/postfacto-deployment/api/manifest-cf4k8s4a8e.yml \
   -p postfacto/package/assets \
-  --hostname retro-temp \
-  -d "${domain}" \
   --var api-app-name=postfacto-api \
-  --var pcf-url="${domain}" \
-  --var domain="${domain}" \
+  --var pcf-url="${cf_domain}" \
+  --var domain="${cf_domain}" \
   --var namespace=postfacto-redis \
   --var redis-password="${redis_password}" \
   --var mysql-address="$MYSQL_ADDRESS" \
-  --var mysql-password="((mysql-password))"
+  --var mysql-password="((mysql-password))" \
+  --no-start
 
-try_curl() {
-  local url
-  url="$1"
-
-  for i in {1..10}; do
-    if curl -k --fail "$url"; then
-      return
-    fi
-    sleep 5
-  done
-
-  echo "curl $url failed"
-  exit 1
-}
-
-try_curl "https://retro-temp.${domain}"
-
-cf map-route postfacto-api "${domain}" --hostname retro
-cf unmap-route postfacto-api "${domain}" --hostname retro-temp
-
-try_curl "https://retro.${domain}"
-
-if cf app postfacto-api-old; then
-  cf unmap-route postfacto-api-old "${domain}" --hostname retro
-  cf delete -f postfacto-api-old
-fi
+cf share-private-domain postfacto eirini.cf
+cf map-route postfacto-api eirini.cf --hostname retro
+cf start postfacto-api
 
 admin_user="((admin-user))"
 admin_password="((admin-password))"
-cf run-task postfacto-api "ADMIN_EMAIL=$admin_user ADMIN_PASSWORD=$admin_password /cnb/lifecycle/launcher -c 'rake admin:create_user'"
+cf run-task postfacto-api -c "ADMIN_EMAIL=$admin_user ADMIN_PASSWORD=$admin_password 'rake admin:create_user'"
--- inline-tasks/helm-install-redis.sh
+++ inline-tasks/helm-install-redis.sh
@@ -5,8 +5,9 @@ set -eu
 export KUBECONFIG="$PWD/kube/config"
 export GOOGLE_APPLICATION_CREDENTIALS="$PWD/kube/service-account.json"
 
-helm init --client-only
 helm repo add bitnami https://charts.bitnami.com/bitnami
-helm upgrade --install postfacto-redis bitnami/redis \
+kubectl create namespace postfacto-redis || true
+helm upgrade postfacto-redis \
+  --install bitnami/redis \
   --namespace postfacto-redis \
   --set securityContext.fsGroup=65531
